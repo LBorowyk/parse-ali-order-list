e5a5803eb20d8942f99ab333b5c3a765126cf7e5
LBorowyk <lborowyk@gmail.com>
2023-12-07 11:19:30 +0200

    remove saved cookie from git

--- parse-order-list.py
+++ parse-order-list.py
@@ -1,43 +1,54 @@
 import pychrome
 from selenium import webdriver
-# from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
 import time
 import pickle
 
+cookie_file_name = 'session'
+
+def dump_cookies(driver):
+    pickle.dump(driver.get_cookies(), open(cookie_file_name, 'wb'))
+    print('Cookies were saving')
+
+def load_cookies(driver):
+    for cookie in pickle.load(open(cookie_file_name, 'rb')):
+        driver.add_cookie(cookie)
+    print('Cookies were loading')
+    driver.refresh()
+
+def init_stealth(driver):
+    stealth(
+        driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True
+    )
+
+def create_chrome_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("start_maximized")
+
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
+    init_stealth(driver)
+    return driver
+
 print('Hello selenium')
-
-options = webdriver.ChromeOptions()
-options.add_argument("start_maximized")
-
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option("useAutomationExtension", False)
-options.headless = True
-driver = webdriver.Chrome(options=options)
-
-stealth(
-    driver,
-    languages=["en-US", "en"],
-    vendor="Google Inc.",
-    platform="Win32",
-    webgl_vendor="Intel Inc.",
-    renderer="Intel Iris OpenGL Engine",
-    fix_hairline=True
-)
+driver = create_chrome_driver()
 
 url = "https://www.aliexpress.com/p/order/index.html"
 driver.get(url)
 
-# time.sleep(30)
 
-# pickle.dump(driver.get_cookies(), open('session', 'wb'))
-# print('Cookies were saving')
 
-for cookie in pickle.load(open('session', 'rb')):
-    driver.add_cookie(cookie)
-print('Cookies were loading')
-driver.refresh()
+
+
 # submit_button = driver.find_element(By.CLASS_NAME, "comet-btn-primary")
 # if submit_button is not None:
 #     submit_button.click()
